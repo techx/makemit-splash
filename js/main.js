99ae3a1ecf3e0c62de560ce8896ef560e603e206
Jack Serrino <Jserrino@gmail.com>
2015-11-25 23:49:31 -0600

    A couple of small changes, fix the gears for most systems

--- js/main.js
+++ js/main.js
@@ -1,37 +1,40 @@
 $(document).ready(function(){
-	$(window).on('scroll', function() {
-		var scrollTop = $(this).scrollTop();
-		var height = $('#home').outerHeight();
-		var calc = (scrollTop / height);
+  $(window).on('scroll', function() {
+    var scrollTop = $(this).scrollTop();
+    var height = $('#home').outerHeight();
+    var calc = (scrollTop / height);
 
-		$('#nav').css({ 'opacity': calc })
-		if ( calc > '1' ) {
-	      	$('#nav').css({ 'opacity': 1 });
-	    } else if ( calc < '0' ) {
-	      	$('#nav').css({ 'opacity': 0 });
-	    }
-	});
+    $('#nav').css({ 'opacity': calc })
+    if ( calc > '1' ) {
+          $('#nav').css({ 'opacity': 1 });
+      } else if ( calc < '0' ) {
+          $('#nav').css({ 'opacity': 0 });
+      }
+  });
 
-	var $cogLeft = $('#cog-left'),
-		$cogRight = $('#cog-right'),
-	    $body = $(document.body),
-	    bodyHeight = $body.height();
-	$(window).scroll(function () {
-	    $cogLeft.css({
-	        'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * -1179 + 31) + 'deg)'
-	    });
-	    $cogRight.css({
-	        'transform': 'rotate(' + ($body.scrollTop() / bodyHeight * 1179 - 31) + 'deg)'
-	    });
-	});	
+  var $cogLeft = $('#cog-left'),
+    $cogRight = $('#cog-right'),
+      $window = $(window),
+      windowHeight = $(window).height();
 
-	setTimeout(function(){
+  var rotateCog = function($cog, amount) {
+    $cog.css({
+      'transform': 'rotate(' + amount + 'deg)'
+    });
+  };
+  $(window).scroll(function () {
+      var amount = ($window.scrollTop() / windowHeight * -1200 + 31);
+      rotateCog($cogLeft, amount);
+      rotateCog($cogRight, -amount);
+  });
+
+  setTimeout(function(){
     var docHeight = $(document).height(),
     tracks = $('#tracks');
     tracks.css({
       'height': docHeight + 'px',
       // '-webkit-animation': 'fadein 1s',
       // 'opacity': '1'
-    }); 
+    });
   }, 2000);
 })
