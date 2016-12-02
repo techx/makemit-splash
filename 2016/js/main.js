0c0128ba72076eccb4a5924f8bad07b347ef1c32
Ravi Rahman <ravi@ravirahman.com>
2016-12-02 15:38:55 -0500

    Cleaned up code
    Signed-off-by: Ravi Rahman <ravi@ravirahman.com>

--- 2016/js/main.js
+++ 2016/js/main.js
@@ -1,70 +1,71 @@
 $(document).ready(function(){
 
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-document.getElementById('gears').style.visibility='hidden';
-document.getElementById('tracks').style.visibility='hidden';
-  } else {
-    $('#nav').hide();
-    $(window).on('scroll', function() {
-      var scrollTop = $(this).scrollTop();
-      var height = $('#home').outerHeight();
-      var calc = (scrollTop / height);
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById('gears').style.visibility='hidden';
+        document.getElementById('tracks').style.visibility='hidden';
+    } else {
+        var nav = $('#nav');
+        nav.hide();
+        $(window).on('scroll', function() {
+            var scrollTop = $(this).scrollTop();
+            var height = $('#home').outerHeight();
+            var calc = (scrollTop / height);
 
-      $('#nav').show();
-      $('#nav').css({ 'opacity': calc });
-      if (calc <= '0') {
-        $('#nav').hide();
-      }
-      if ( calc > '1' ) {
-        $('#nav').css({ 'opacity': 1 });
-      } else if ( calc < '0' ) {
-        $('#nav').css({ 'opacity': 0 });
-      }
-    });
 
-    var $cogLeft = $('#cog-left'),
-    $cogRight = $('#cog-right'),
-    $window = $(window),
-    windowHeight = $(window).height();
+            nav.show();
+            nav.css({ 'opacity': calc });
+            if (calc <= '0') {
+                nav.hide();
+            }
+            if ( calc > '1' ) {
+                nav.css({ 'opacity': 1 });
+            } else if ( calc < '0' ) {
+                nav.css({ 'opacity': 0 });
+            }
+        });
 
-    var rotateCog = function($cog, amount) {
-      $cog.css({
-        'transform': 'rotate(' + amount + 'rad)'
-      });
-    };
-    $(window).scroll(function () {
-      var amount = ($window.scrollTop() / 47.13 + 0.25);
-      rotateCog($cogLeft, -amount);
-      rotateCog($cogRight, amount);
-    });
+        var $cogLeft = $('#cog-left'),
+            $cogRight = $('#cog-right'),
+            $window = $(window);
 
-    setTimeout(function(){
-      var docHeight = $(document).height(),
-      tracks = $('#tracks');
-      tracks.css({
-        'height': docHeight + 'px',
-        // '-webkit-animation': 'fadein 1s',
-        // 'opacity': '1'
-      });
-    }, 2000);
+        var rotateCog = function($cog, amount) {
+            $cog.css({
+                'transform': 'rotate(' + amount + 'rad)'
+            });
+        };
+        $(window).scroll(function () {
+            var amount = ($window.scrollTop() / 47.13 + 0.25);
+            rotateCog($cogLeft, -amount);
+            rotateCog($cogRight, amount);
+        });
 
-    $(window).resize(function() {
-      tracks = $('#tracks');
-      tracks.css({
-        'height': '0px'
-      });
-      var docHeight = $(document).height();
-      tracks.css({
-        'height': docHeight + 'px',
-      });
-    });
-  }
+        setTimeout(function(){
+            var docHeight = $(document).height(),
+                tracks = $('#tracks');
+            tracks.css({
+                'height': docHeight + 'px'
+                // '-webkit-animation': 'fadein 1s',
+                // 'opacity': '1'
+            });
+        }, 2000);
+
+        $(window).resize(function() {
+            var tracks = $('#tracks');
+            tracks.css({
+                'height': '0px'
+            });
+            var docHeight = $(document).height();
+            tracks.css({
+                'height': docHeight + 'px'
+            });
+        });
+    }
 });
 
 
 /*
-* Email interest list for volunteering
-*/
+ * Email interest list for volunteering
+ */
 
 
 
--- js/main.js
+++ js/main.js
@@ -1,79 +1,79 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    var nav = $('#nav');
+    var video = $("#logo-vid");
+    var tracks = $('#tracks');
 
-  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
-document.getElementById('gears').style.visibility='hidden';
-document.getElementById('tracks').style.visibility='hidden';
- $('.nav').css('display', 'none');
+    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        document.getElementById('gears').style.visibility='hidden';
+        document.getElementById('tracks').style.visibility='hidden';
+        nav.css('display', 'none');
 
-// android / ios autoplay video
- var video = document.querySelector('#logo-vid');
- $("#logo-vid").css("margin-left", "-420px");
- window.addEventListener('touchstart', function videoStart() {
-   video.play();
-   this.removeEventListener('touchstart', videoStart);
- });
+        // android / ios autoplay video
+        video.css("margin-left", "-420px");
+        window.addEventListener('touchstart', function videoStart() {
+            video.play();
+            this.removeEventListener('touchstart', videoStart);
+        });
 
-  } else {
-    $('.nav').css('display', 'none');
-    $(window).on('scroll', function() {
-      var scrollTop = $(this).scrollTop();
-      var height = $('.nav').height();
-      var wHeight = $(window).height();
-      console.log({"st": scrollTop, "comp": wHeight+height});
-      if (scrollTop>wHeight+height+20) {
-        $(".nav").removeClass("nav-abs");
-        $(".nav").addClass("nav-fixed");
-        $(".nav").css("top", "0px");
-      }
-      else {
-        $(".nav").removeClass("nav-fixed");
-        $(".nav").addClass("nav-abs");
-        var makeMitPos = $("#title-accent").position().top;
-        $(".nav").css("top", $($(".hsContainer")[1]).position().top + "px");
-      }
-    });
+    } else {
+        nav.css('display', 'none');
+        console.log("nav css is display none");
+        $(window).on('scroll', function() {
+            var scrollTop = $(this).scrollTop();
+            var height = nav.height();
+            var wHeight = $(window).height();
+            console.log({"st": scrollTop, "comp": wHeight+height});
+            if (scrollTop>wHeight+height+20) {
+                nav.removeClass("nav-abs");
+                nav.addClass("nav-fixed");
+                nav.css("top", "0px");
+            }
+            else {
+                nav.removeClass("nav-fixed");
+                nav.addClass("nav-abs");
+                nav.css("top", $($(".hsContainer")[1]).position().top + "px");
+            }
+        });
 
-    var $cogLeft = $('#cog-left'),
-    $cogRight = $('#cog-right'),
-    $window = $(window),
-    windowHeight = $(window).height();
+        var $cogLeft = $('#cog-left'),
+            $cogRight = $('#cog-right'),
+            $window = $(window);
 
-    var rotateCog = function($cog, amount) {
-      $cog.css({
-        'transform': 'rotate(' + amount + 'rad)'
-      });
-    };
-    $(window).scroll(function () {
-      var amount = ($window.scrollTop() / 101.9 + 0);
-      // var amount = ($window.scrollTop() / 40.13 + 0.55);
-      rotateCog($cogLeft, -amount);
-      rotateCog($cogRight, amount);
-    });
+        var rotateCog = function($cog, amount) {
+            $cog.css({
+                'transform': 'rotate(' + amount + 'rad)'
+            });
+        };
+        $(window).scroll(function () {
+            var amount = $window.scrollTop() / 101.9;
+            // var amount = ($window.scrollTop() / 40.13 + 0.55);
+            rotateCog($cogLeft, -amount);
+            rotateCog($cogRight, amount);
+        });
 
-    setTimeout(function(){
-      var docHeight = $(document).height(),
-      tracks = $('#tracks');
-      tracks.css({
-        'height': docHeight + 'px',
-        // '-webkit-animation': 'fadein 1s',
-        // 'opacity': '1'
-      });
-    }, 2000);
+        setTimeout(function(){
+            var docHeight = $(document).height();
 
-    $(window).resize(function() {
-      tracks = $('#tracks');
-      tracks.css({
-        'height': '0px'
-      });
-      var docHeight = $(document).height();
-      tracks.css({
-        'height': docHeight + 'px',
-      });
-    });
-  }
+            tracks.css({
+                'height': docHeight + 'px'
+                // '-webkit-animation': 'fadein 1s',
+                // 'opacity': '1'
+            });
+        }, 2000);
+
+        $(window).resize(function() {
+            tracks.css({
+                'height': '0px'
+            });
+            var docHeight = $(document).height();
+            tracks.css({
+                'height': docHeight + 'px'
+            });
+        });
+    }
 });
 
 
 /*
-* Email interest list for volunteering
-*/
+ * Email interest list for volunteering
+ */
